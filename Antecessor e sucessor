6ecf2bbcfef93f94c3cf974e1f3da2fc0b0bafad
devjoaquimsouza <souza.joaquimdev@proton.me>
2025-06-18 19:19:06 -0300

    Update and rename desafio005.py to Antecessor e sucessor

---
+++ Antecessor e sucessor
@@ -0,0 +1,4 @@
+n1 = int(input('Informe um número inteiro: '))
+sucessor = n1 + 1
+antecessor = n1 - 1
+print('O número escolhido foi esse: {} \n Seu antecessor é >> {} \n Seu sucessor é >> {}'.format(n1, antecessor, sucessor))
--- desafio005.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Informe um número inteiro: '))
-sucessor = n1 + 1
-antecessor = n1 - 1
-print('O número escolhido foi esse: {} \n Seu antecessor é >> {} \n Seu sucessor é >> {}'.format(n1, antecessor, sucessor))
