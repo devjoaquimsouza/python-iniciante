292d4f12823ed5929f04f611c401a45d83bbd029
devjoaquimsouza <souza.joaquimdev@proton.me>
2025-06-20 17:20:00 -0300

    Update and rename desafio008.py to 06 - Metros em Centímetros e Milímetros

--- desafio008.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input('Informe um valor em metros: '))
-cent = n1 * 100
-mil = n1 * 1000
-print('A metragem informada é igual a {} m \n Em centímetros >> {} cm \n Em milímetros >> {} mm'. format(n1, cent, mil))
